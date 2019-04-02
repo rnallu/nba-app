dba3377185152f7d3931acde7c523c0e0c00dcd0
Ramesh <rameshnaidu.allu@gmail.com>
2019-04-02 15:40:51 +0530

    Blank commit
    blank commit to update

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import Layout from './hoc/Layout/layout';
-import Home from './components/Home/home';
+import Layout from "./hoc/Layout/layout";
+import Home from "./components/Home/home";
 
 class Routes extends React.Component {
-
-render() {
-        return (
-                <Layout>
-                    <BrowserRouter>   
-                        <Route path='/' component={Home}/>
-                    </BrowserRouter>
-                </Layout>
-        )
-    }
+  render() {
+    return (
+      <Layout>
+        <BrowserRouter>
+          <Route path="/" component={Home} />
+        </BrowserRouter>
+      </Layout>
+    );
+  }
 }
 
 export default Routes;
