4ec0dd7c5d14117e411ad57ac985d5bd4c62dcc6
Ramesh <rameshnaidu.allu@gmail.com>
2019-04-14 12:47:00 +0530

    news list transitions added

--- src/components/Widgets/NewsList/newsList.js
+++ src/components/Widgets/NewsList/newsList.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 
 import {URL} from '../../../config';
 import './newsList.css';
 
 class NewsList extends React.Component {
-    constructor(props){
-        super(props);
 
-        this.state = {
+        state = {
             items:[],
             start:3,
             end:6
         }
-    }
-    componentWillMount(){
+    
+    componentDidMount(){
         this.request(3,6);
     }
 
@@ -30,22 +29,30 @@ class NewsList extends React.Component {
 
     loadNews = () => {
         return this.state.items.map((item, i)=> 
-            <div key={i}>
+            <CSSTransition 
+                key={i}
+                timeout={2000}
+                classNames="list"
+            >
                 <div className="newsList_item">
                   <Link to={`/articles/${item.id}`}>  <h2>{item.title}</h2> </Link></div>
-            </div> )
+            </CSSTransition>
+             )
+    }
+    
+    loadMore = () => {
+    this.request(this.state.end, this.state.end + 3)
+    this.setState({ start: this.state.end, end: this.state.end + 3 })
+    
     }
 
-    loadMore = () => {
-        this.setState({start:this.state.end});
-        this.setState({end:this.state.start+3});
-        this.request(this.state.start,this.state.end);
-    }
     render () {  
     
         return (
             <div>
+                <TransitionGroup component="h2">
                 {this.loadNews()}
+                </TransitionGroup>
                 <div className="loadmore" onClick={this.loadMore}>Load More</div>
             </div>
         )
