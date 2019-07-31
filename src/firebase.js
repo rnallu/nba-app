58c648d5e18316a6aaebed12c37191810b0f7270
Ramesh <rameshnaidu.allu@gmail.com>
2019-07-31 17:46:45 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,6 @@
 import * as firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDpFrbllEH7YEq6CP8EbrXbi1L9r4PmULM",
-  authDomain: "my-react-nba.firebaseapp.com",
-  databaseURL: "https://my-react-nba.firebaseio.com",
-  projectId: "my-react-nba",
-  storageBucket: "my-react-nba.appspot.com",
-  messagingSenderId: "558096658987",
-  appId: "1:558096658987:web:c15bf9b130b29a12"
-};
+import firebaseConfig from "../firebaseCred";
 
 firebase.initializeApp(firebaseConfig);
 
